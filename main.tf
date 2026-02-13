829a32ed6a2d0b804238c55f9b29bb55c1565208
Zack Wang <zack.wang@geckorobotics.com>
2026-02-13 12:42:54 -0500

    remove requires

--- main.tf
+++ main.tf
@@ -85,7 +85,6 @@ data "cloudinit_config" "config" {
           Description=Start atlantis container
           Wants=atlantis-chown-disk.service
           After=atlantis-chown-disk.service google-startup-scripts.service
-          Requires=google-startup-scripts.service
           [Service]
           Environment=HOME=/home/atlantis
           ExecStartPre=/usr/bin/docker-credential-gcr configure-docker --registries ${var.region}-docker.pkg.dev
