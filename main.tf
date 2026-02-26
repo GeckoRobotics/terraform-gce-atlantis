c07769d2712f6cc1e8993eaffc52974fdc4eadf2
Zack Wang <zack.wang@geckorobotics.com>
2026-02-26 17:52:26 -0500

    fix

--- main.tf
+++ main.tf
@@ -561,7 +561,6 @@ resource "google_compute_region_backend_service" "iap_regional" {
   connection_draining_timeout_sec = 5
   load_balancing_scheme           = "EXTERNAL_MANAGED"
   health_checks                   = [google_compute_region_health_check.default_regional[0].id]
-  security_policy                 = var.iap_backend_security_policy
 
   log_config {
     enable      = true
