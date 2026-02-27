e127fac5c7eeea1c627fdce1f2eaeaa6f8f3b954
Zack Wang <zack.wang@geckorobotics.com>
2026-02-27 09:44:38 -0500

    fix

--- main.tf
+++ main.tf
@@ -547,6 +547,8 @@ resource "google_compute_region_backend_service" "default_regional" {
     balancing_mode  = "UTILIZATION"
     max_utilization = 0.8
     group           = google_compute_instance_group_manager.default.instance_group
+    capacity_scaler = 1
+    failover        = false
   }
   project = var.project
 }
